e5fa092e85eed7fd5414ed2ca59858461e062da0
Orion Quick <orionquick@gmail.com>
2020-04-10 17:15:51 -0700

    added more tileset

---
+++ Assets/LayerControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerControl : MonoBehaviour
+{
+
+    public SpriteRenderer sr;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        sr.sortingOrder = Mathf.RoundToInt(transform.position.y * 100f) * -1;
+    }
+}
--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -5,76 +5,62 @@ using UnityEditor.Animations;
 
 public class Movement : MonoBehaviour
 {
-    public GameObject obj;
-    public Camera mainCam;
-    public float speed = 0.1f;
-    public float lookAngle = 0;
-    public float walkAngle = 0;
-    public float mouseX = 0;
-    public float mouseY = 0;
+    public float walkSpeed = 0.1f;
+    public Camera activeCam;
     public Rigidbody2D rigidBody;
     public SpriteRenderer spriteRenderer;
     public Animator animator;
 
     public void FixedUpdate()
     {
-        spriteRenderer.sortingOrder = Mathf.RoundToInt(obj.transform.position.y * 100f) * -1;
-
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
 
-        Vector2 mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePos = activeCam.ScreenToWorldPoint(Input.mousePosition);
 
-        mouseX = mousePos.x;
-        mouseY = mousePos.y;
+        float mouseX = mousePos.x;
+        float mouseY = mousePos.y;
 
-        lookAngle = Vector2.Angle(new Vector2(0, 0 - 1), new Vector2(mouseX - obj.transform.position.x, mouseY - obj.transform.position.y));
-        walkAngle = Vector2.Angle(new Vector2(0, -1), new Vector2(h, v));
+        float lookAngle = Mathf.Round(Vector2.Angle(new Vector2(0, 0 - 1), new Vector2(mouseX - transform.position.x, mouseY - transform.position.y)) / 45) * 45;
+        float walkAngle = Vector2.Angle(new Vector2(0, -1), new Vector2(h, v));
 
-        /*
-        if (h < 0) spriteRenderer.flipX = true;
-        if (h > 0) spriteRenderer.flipX = false;
+        float speed = walkSpeed;
 
-        if (h == 0 && v < 0)    animator.runtimeAnimatorController = S;
-        if (h > 0 && v < 0)     animator.runtimeAnimatorController = SE;
-        if (h > 0 && v == 0)    animator.runtimeAnimatorController = E;
-        if (h > 0 && v > 0)     animator.runtimeAnimatorController = NE;
-        if (h == 0 && v > 0)    animator.runtimeAnimatorController = N;
-        if (h < 0 && v > 0)     animator.runtimeAnimatorController = NE;
-        if (h < 0 && v == 0)    animator.runtimeAnimatorController = E;
-        if (h < 0 && v < 0)     animator.runtimeAnimatorController = SE;
-        */
+        if (h < 0) walkAngle = 360 - walkAngle;
 
-        if (h < 0) walkAngle = -walkAngle;
-
-        if (mouseX < obj.transform.position.x) { spriteRenderer.flipX = true; lookAngle = -lookAngle;}
-        if (mouseX > obj.transform.position.x) spriteRenderer.flipX = false;
+        if (mouseX < transform.position.x) {
+            spriteRenderer.flipX = true;
+            lookAngle = 360 - lookAngle;
+        }
+        if (mouseX > transform.position.x) {
+            spriteRenderer.flipX = false;
+        }
 
         if (h == 0 && v == 0) {
-            if      (Mathf.Abs(lookAngle) < 22.5)  animator.Play("south_idle");
-            else if (Mathf.Abs(lookAngle) < 67.5)  animator.Play("southeast_idle");
-            else if (Mathf.Abs(lookAngle) < 112.5) animator.Play("east_idle");
-            else if (Mathf.Abs(lookAngle) < 157.5) animator.Play("northeast_idle");
-            else if (Mathf.Abs(lookAngle) < 180)   animator.Play("north_idle");
+            if (lookAngle == 0 || lookAngle == 360)         animator.Play("south_idle");
+            else if (lookAngle == 45 || lookAngle == 315)   animator.Play("southeast_idle");
+            else if (lookAngle == 90 || lookAngle == 270)   animator.Play("east_idle");
+            else if (lookAngle == 135 || lookAngle == 225)  animator.Play("northeast_idle");
+            else if (lookAngle == 180)                      animator.Play("north_idle");
         }
         else {
-            if (Mathf.Abs(lookAngle - walkAngle) <= 90) {
-                speed = 0.1f;
+            if (Mathf.Abs(lookAngle - walkAngle) <= 90 || Mathf.Abs(lookAngle - walkAngle) > 270) {
+                speed = walkSpeed;
 
-                if      (Mathf.Abs(lookAngle) < 22.5)  animator.Play("south_walk");
-                else if (Mathf.Abs(lookAngle) < 67.5)  animator.Play("southeast_walk");
-                else if (Mathf.Abs(lookAngle) < 112.5) animator.Play("east_walk");
-                else if (Mathf.Abs(lookAngle) < 157.5) animator.Play("northeast_walk");
-                else if (Mathf.Abs(lookAngle) < 180)   animator.Play("north_walk");
+                if (lookAngle == 0 || lookAngle == 360)         animator.Play("south_walk");
+                else if (lookAngle == 45 || lookAngle == 315)   animator.Play("southeast_walk");
+                else if (lookAngle == 90 || lookAngle == 270)   animator.Play("east_walk");
+                else if (lookAngle == 135 || lookAngle == 225)  animator.Play("northeast_walk");
+                else if (lookAngle == 180)                      animator.Play("north_walk");
             }
             else {
-                speed = 0.08f;
+                speed = walkSpeed * 0.6f;
 
-                if      (Mathf.Abs(lookAngle) < 22.5)  animator.Play("south_walk_reverse");
-                else if (Mathf.Abs(lookAngle) < 67.5)  animator.Play("southeast_walk_reverse");
-                else if (Mathf.Abs(lookAngle) < 112.5) animator.Play("east_walk_reverse");
-                else if (Mathf.Abs(lookAngle) < 157.5) animator.Play("northeast_walk_reverse");
-                else if (Mathf.Abs(lookAngle) < 180)   animator.Play("north_walk_reverse");
+                if (lookAngle == 0 || lookAngle == 360)         animator.Play("south_walk_reverse");
+                else if (lookAngle == 45 || lookAngle == 315)   animator.Play("southeast_walk_reverse");
+                else if (lookAngle == 90 || lookAngle == 270)   animator.Play("east_walk_reverse");
+                else if (lookAngle == 135 || lookAngle == 225)  animator.Play("northeast_walk_reverse");
+                else if (lookAngle == 180)                      animator.Play("north_walk_reverse");
             }
         }
 
